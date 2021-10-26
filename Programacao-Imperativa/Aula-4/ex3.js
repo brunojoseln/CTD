afa38a31a3db93d164363e6103cb1e680b9ab759
brunojoseln <brunojoseln@gmail.com>
2021-10-25 23:14:38 -0300

    primeiro commmit

---
+++ Programacao-Imperativa/Aula-4/ex3.js
@@ -0,0 +1,22 @@
+// 1
+let x = 10; 
+let y = 5;  
+console.log (x + y); 
+// 2
+console.log (x * y);  
+// 3
+var num1 = 100;
+var num2 = 200;
+var somanum = num1 + num2
+console.log (somanum); 
+// 4
+let z = 2  
+console.log (x + y + z);   
+// 5
+z = "1" + 1; 
+console.log(z)  
+//6
+z = "olá" + 23; 
+console.log(z)
+// 7
+console.log (x +" e "+ y);
