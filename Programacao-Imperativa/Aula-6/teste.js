33fa348345ad567c8186f01fdb3669d4f5ce071e
brunojoseln <brunojoseln@gmail.com>
2021-11-03 20:32:15 -0300

    exercicios booleanos adicionados

---
+++ Programacao-Imperativa/Aula-6/teste.js
@@ -0,0 +1,27 @@
+// function anterior(num){
+//     return --num
+//   }
+  
+// console.log(anterior(100))
+// function anterior(num){
+//     return --num
+//   }
+  
+//   function triplo(num){
+//     return num*3
+//   }
+// function anteriorDoTriplo(num){
+//     return anterior(triplo(num))  
+//   }
+
+// console.log(anteriorDoTriplo(100))
+
+var salario = 50000;
+var cargo = salario <= 1000 ? 'junior' : salario <= 5000 ? 'senior' : 'diretor';
+console.log(cargo)
+
+console.log(3003%2);
+
+function medalhaDeAcordoComPosto(num){
+    return num === 1 ? "ouro" : num === 2 ? "prata" : num === 3 ? "bronze" : "nada"
+  }
---
+++ Programacao-Imperativa/Aula-7/Exercicios.js
@@ -0,0 +1,32 @@
+console.log(true)
+console.log(!false)
+console.log(!!false)
+console.log(!!true)
+console.log(!1)
+console.log(!!1)
+console.log(!0)
+console.log(!!0)
+console.log(!!"") 
+let x = 5
+let y = 9
+console.log(x < 10 && x!==5)
+console.log(x>9 || x===5)
+console.log(!(x===y))
+
+// Parte 2 -----------------------
+
+x = 10
+y ="a"
+console.log(y==="b" || x >= 10)
+
+
+x=10
+y ="a"
+console.log(y==="b" || x >= 10)
+
+let str = ""
+let msg = "haha!"
+let eBonito = "false"
+console.log(!((str || msg) && eBonito))
+
+console.log(!-10);
